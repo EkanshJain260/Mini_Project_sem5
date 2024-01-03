1dc0a6fe882b09e288d8745a5000c019f91e8453
Ekansh <ekansh.21b1541146@abes.ac.in>
2024-01-03 12:50:42 +0530

    initial commit

---
+++ components/index.ts
@@ -0,0 +1,24 @@
+import CarCard from "./CarCard";
+import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
+import Footer from "./Footer";
+import NavBar from "./Navbar";
+import ShowMore from "./ShowMore";
+import SearchBar from "./SearchBar";
+import Hero from "./Hero";
+import CarDetails from "./CarDetails";
+import SearchManufacturer from "./SearchManufacturer";
+
+
+export {
+    Hero,
+    CarCard,
+    CustomButton,
+    CustomFilter,
+    Footer,
+    NavBar,
+    ShowMore,
+    SearchBar,
+    CarDetails,
+    SearchManufacturer
+};
